1ede102dedb3d6de6b61d13af642269c72f10a24
Raul <Raul_prisecaru123@protonmail.com>
2024-04-10 03:32:11 +0100

    Fixed SQL file failing from creating tables

--- MainMenu.py
+++ MainMenu.py
@@ -12,17 +12,19 @@ def setup_database():
         connection = sqlite3.connect("Database/CentralisedDatabase.db")
         cursor = connection.cursor()
 
-        with open("Database/CentralisedDatabase.sql", "r") as CentralisedDatabaseFile:
-            sql_script = CentralisedDatabaseFile.read()
+        with open("Database/CentralisedDatabase.sql", "r") as sql_file:
+            sql_script = sql_file.read()
 
-        cursor.execute(sql_script)
+        cursor.executescript(sql_script)
 
         connection.commit()
         connection.close()
+        print("Database Successfully Created")
+
     except NameError:
         print("NameError Caught: Database (.db) Not Found.")
-    except:
-        print("Error Caught: Database (.db) Accessable but couldn't execute SQL")
+    except Exception as e:
+        print("Error Caught: " + str(e))
 
 
 if __name__ == '__main__':
