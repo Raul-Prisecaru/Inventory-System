b784b1098035e135c630626c4c5d851da3d258a6
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-04-10 19:44:39 +0100

    Completed Randomise Inventory Table System

--- RandomiseDatabase.py
+++ RandomiseDatabase.py
@@ -1,7 +1,10 @@
 import sqlite3
+import random
 
+nameList = []
+locationList = []
 
-def randomiseDatabase():
+def randomiseInventory():
     try:
         connection = sqlite3.connect('Database/CentralisedDatabase.db')
         cursor = connection.cursor()
@@ -13,8 +16,20 @@ def randomiseDatabase():
 
         showcase_query = "SELECT * FROM Inventory;"
 
-        for row in range(1000):
-            cursor.execute(select_query, ("asd", 123, "dfg"))
+        with open("TextFile/NameRecords.txt", "r") as file:
+            for line in file:
+                nameList.append(line)
+                line.strip()
+
+        with open("TextFile/LocationBuilding.txt", "r") as file:
+            for line in file:
+                locationList.append(line)
+                line.strip()
+
+
+
+        for row in range(5):
+            cursor.execute(select_query, (random.choice(nameList), random.randint(30, 400), random.choice(locationList)))
 
         cursor.execute(showcase_query)
 
@@ -26,9 +41,9 @@ def randomiseDatabase():
         connection.close()
 
     except NameError:
-        print("Error Caught: Database Not Found.")
+        print("Error Caught: File Not Found.")
     except Exception as e:
         print("Something Else Went Wrong: " + str(e))
 
 if __name__ == '__main__':
-    randomiseDatabase()
+    randomiseInventory()
