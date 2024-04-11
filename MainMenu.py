811c42bd5ea10bb403393f3fd635804b78fc94a1
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-04-11 18:15:07 +0100

    Created Basic Window

--- MainMenu.py
+++ MainMenu.py
@@ -1,11 +1,12 @@
 import sqlite3
-LogisticSystemOption = int(input("""Welcome to St Mary's Logistic System
-What would you like to do?
-[1] - Add New Inventory
-[2] - Manage Inventory
-[3] - Track All Shipment
-[4] - Generate Reports for Inventory Status
-:: """))
+import tkinter as tk
+
+def create_window():
+    window = tk.Tk()
+    window.geometry("600x500")
+    window.title("St Mary's Logistic System")
+    window.mainloop()
+
 
 def setup_database():
     try:
@@ -28,18 +29,5 @@ def setup_database():
 
 
 if __name__ == '__main__':
-    match LogisticSystemOption:
-        case 1:
-            print("You have selected 1")
-            setup_database()
-        case 2:
-            print("You have selected 2")
-
-        case 3:
-            print("You have selected 3")
-
-        case 4:
-            print("You have selected 4")
-
-        case _:
-            print("Invalid Option was selected")
+        create_window()
+        setup_database()
