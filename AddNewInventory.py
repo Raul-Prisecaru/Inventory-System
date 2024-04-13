3fa3aa7fc0cee57c85e3700828363512c7ca2a48
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-04-13 02:18:36 +0100

    Added the system so that then the user selected to add Inventory then displays all relevant columns + Reworking on AddNewInvetory.py functionally to make it more clearer

--- AddNewInventory.py
+++ AddNewInventory.py
@@ -1,21 +1,41 @@
 import sqlite3
 
 
-def addNewInventory(Table):
-    global row
+def getAllColumns(Table):
     columnList = []
+
+    connection = sqlite3.connect("Database/CentralisedDatabase.db")
+    cursor = connection.cursor()
+    selectQuery = cursor.execute(f"SELECT * FROM {Table}")
+
+    for row in selectQuery.description:
+        columnList.append(row[0])
+
+    return columnList
+
+
+def getPlaceholders(Table, Values):
+    connection = sqlite3.connect("Database/CentralisedDatabase.db")
+    cursor = connection.cursor()
+
+    cursor.execute(f"SELECT * FROM {Table}")
+    columns = cursor.fetchall()
+
+def addNewInventory(Table, values):
+    columnList = []
+    # global row
     connection = sqlite3.connect("Database/CentralisedDatabase.db")
     cursor = connection.cursor()
 
     #Get Information About Columns
     cursor.execute(f"PRAGMA table_info({Table});")
-    selectQuery = cursor.execute(f"SELECT * FROM {Table}")
     columns = cursor.fetchall()
 
     # Count the columns in a table
     numberColumns = len(columns)
     print(numberColumns)
 
+    selectQuery = cursor.execute(f"SELECT * FROM {Table}")
     # Add placeholders with .join with a ", " for the number of columns.
     totalValues = ", ".join('?' for _ in range(numberColumns))
     print(totalValues)
@@ -31,7 +51,7 @@ def addNewInventory(Table):
     # INSERT INTO {Table that user provides} ({All of the columns available in the table}) VALUES ({add placeholders per column})
     select_query = f"INSERT INTO {Table} ({allColumnNames}) VALUES ({totalValues})"
 
-    cursor.execute(select_query, ("1", "1", "1", "1", "1"))
+    cursor.execute(select_query, f"{values}")
 
     connection.commit()
     connection.close()
