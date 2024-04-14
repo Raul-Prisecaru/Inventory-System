83a9a6f06494bce0e5b3de0f4edfdb981c62f550
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-04-14 23:32:40 +0100

    Created basic Modify Inventory

--- ModifyInventory.py
+++ ModifyInventory.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def getAllColumns(Table):
     columnList = []
 
@@ -10,32 +11,50 @@ def getAllColumns(Table):
     for row in selectQuery.description:
         columnList.append(row[0])
 
+    # pop(0) removes the first item in the list
+    columnList.pop(0)
     return columnList
 
 
 def getPlaceholders(Table):
+    placeholderValues = []
     connection = sqlite3.connect("Database/CentralisedDatabase.db")
     cursor = connection.cursor()
 
     cursor.execute(f"SELECT * FROM {Table}")
     columns = cursor.fetchall()
 
-    Values = len(columns[0])
+    Values = len(columns[0] - 1)
     print(Values)
 
-    placeholderValues = ", ".join('?' for _ in range(Values))
+    for _ in range(Values):
+        placeholderValues.append(", ".join("?"))
     print(placeholderValues)
 
     return placeholderValues
 
-def modifyInventory(Table, values, ID):
+
+def modifyOneInventory(Table, values, row):
     connection = sqlite3.connect("Database/CentralisedDatabase.db")
-    cursor = connection.cursor();
+    cursor = connection.cursor()
+
+
+def modifyAllInventory(Table, values):
+    connection = sqlite3.connect("Database/CentralisedDatabase.db")
+    cursor = connection.cursor()
 
     columns = getAllColumns(Table)
-    Placeholders = getPlaceholders(Table)
+    # placeholders = getPlaceholders(Table)
 
-    AlterQuery = f"""UPDATE {Table} SET {columns} = {Placeholders} WHERE '%ID' = {ID}"""
+    # Construct the SET clause dynamically
+    for column in columns:
+        setClause = ', '.join(f'{column} = ?')
+
+    # Construct the UPDATE query
+    update_query = f"UPDATE {Table} SET {setClause} WHERE NOT LIKE '%ID'"
+
+    # Execute the query
+    cursor.execute(update_query, values)
 
     connection.commit()
     connection.close()
