2e936c2c8d0550f17f47197c0be7ebcbf96ad354
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-04-16 23:03:50 +0100

    ModifyInventory.py: Cleaned up code, Added Comments

--- ModifyInventory.py
+++ ModifyInventory.py
@@ -1,79 +1,89 @@
 import sqlite3
 
 
-def getAllColumns(Table):
+def getAllColumnsByID(Table):
+    # List
     columnList = []
+
+    # Connect to Database
     connection = sqlite3.connect('Database/CentralisedDatabase.db')
     cursor = connection.cursor()
 
+    # Get all information from Table and execute
     executeQuery = f'SELECT * FROM {Table}'
     rows = cursor.execute(executeQuery)
 
+    # Get all available Information from a table by using .description
     for row in rows.description:
         columnList.append(row)
+    print(columnList)
 
+    # Get the Column Name (First in the list)
     columnID = columnList[0]
     return columnID[0]
 
 
 def getAllColumnsNoID(Table):
+    # List
     columnList = []
 
+    # Connect to Database
     connection = sqlite3.connect("Database/CentralisedDatabase.db")
     cursor = connection.cursor()
     selectQuery = cursor.execute(f"SELECT * FROM {Table}")
 
+    # Get all information from Table and execute
     for row in selectQuery.description:
         columnList.append(row[0])
 
-    # pop(0) removes the first item in the list
+    # Remove the first column (ID column) with pop(0)
     columnList.pop(0)
     return columnList
 
 
 def getPlaceholdersNoID(Table):
+    # List
     placeholderValues = []
+
+    # Connect to Database
     connection = sqlite3.connect("Database/CentralisedDatabase.db")
     cursor = connection.cursor()
 
+    # Get all results from table and use fetchall to store each row as a tuple
     cursor.execute(f"SELECT * FROM {Table}")
     columns = cursor.fetchall()
-    # Get number of columns from Table then subtract -1 to not include ID
-    Values = len(columns[0] - 1)
-    print(Values)
+    print(columns)
 
+    # Get number of columns from Table then subtract -1 to not include ID
+    Values = len(columns[0]) - 1
+
+    # Used to format all the Placeholders in a list to be later used in the SQL query
     for _ in range(Values):
         placeholderValues.append(", ".join("?"))
-    print(placeholderValues)
 
+    # Return list with placeholders
     return placeholderValues
 
 
-def modifyOneInventory(Table, values, row):
-    connection = sqlite3.connect("Database/CentralisedDatabase.db")
-    cursor = connection.cursor()
-
-
 def modifyAllInventory(Table, values, ID):
-    GetColumnID = getAllColumns(Table)
-    print(GetColumnID)
+    # Store the ID column
+    GetColumnID = getAllColumnsByID(Table)
+
+    # Connect To Database
     connection = sqlite3.connect("Database/CentralisedDatabase.db")
     cursor = connection.cursor()
 
+    # Store all Columns Except ID
     columns = getAllColumnsNoID(Table)
 
+    # Create Format for UPDATE Query depending on number of columns: ColumnName = ?, ColumnName = ?
     updateFormat = ', '.join([f'{column} = ?' for column in columns])
 
-    getID = f'SELECT * FROM {Table} WHERE'
-
+    # UPDATE {Table provided by user} SET {Format Created Above} WHERE {ColumnID} = {ID provided by user}
     updateQuery = f'UPDATE {Table} SET {updateFormat} WHERE {GetColumnID} = {ID}'
 
-    # cursor.execute(viewQuery)  # Execute the CREATE VIEW statement
-    cursor.execute(updateQuery, values)  # Execute the UPDATE query
+    # Execute SQLQuery and values that user provides
+    cursor.execute(updateQuery, values)
 
     connection.commit()
     connection.close()
-
-
-if __name__ == '__main__':
-    print(getAllColumns('Drivers'))
