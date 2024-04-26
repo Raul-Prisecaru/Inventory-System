c45f059d9238502e180471b1a37d3cd9a21c6a67
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-04-27 00:38:47 +0100

    AddInventoryPage.py: Cleaned Up Code + Implemented Comments

--- Pages/AddInventoryPage.py
+++ Pages/AddInventoryPage.py
@@ -10,9 +10,8 @@ current_directory = os.path.dirname(__file__)
 # Construct the path to the database file relative to the current directory
 database_path = os.path.join(current_directory, '..', 'Database', 'CentralisedDatabase.db')
 
+# Used to store user response to add to the DB
 components = {}
-options = []
-userOption = ''
 
 
 # Function to get all of the tables found in the DB
@@ -35,6 +34,7 @@ def getAllTables():
     tableinfo.remove("sqlite_sequence")
     tableinfo.remove("LoginInformation")
 
+    # Return List to display the tables to the user later on
     return tableinfo
 
 
@@ -55,81 +55,67 @@ def MainWindowConfig():
 
 # Function to create options for user to interact with DB
 def DBInteract(window):
-    # Global Variables to interact outside function
-    global tableOptions2
-    global userOption
+    # Responsible for managing options shown in Combobox
+    global tableOptionComboBox
 
-    # Function to create and display components on the window
-    def displayComponents():
-        global userOption
-        columns = getAllColumns(tableOptions2.get())
-        userOption = tableOptions2.get()
-        print(f'tableOptions2: {tableOptions2.get()}')
-        for column in columns:
-            columnLabel = tk.Label(window, text=f'{column}')
-            columnLabel.pack()
-
-            columnEntry = tk.Entry(window)
-            columnEntry.pack()
-
-            components[column] = columnEntry
-
-        addInventoryButton = tk.Button(window, text='Add to table')
-        addInventoryButton.pack()
-        addInventoryButton.bind('<ButtonRelease-1>', onAddInventoryPress)
-
+    # Create label to inform user what dropbox is responsible for
     tableLabel = tk.Label(window, text='Which Table would you like to deal with?')
     tableLabel.pack()
 
-    # Create the dropdown table options
+    # Get all the tables available from getAllTables() and print for debugging purposes.
     functionTableOption = getAllTables()
     print(f'functionTableOption: {functionTableOption}')
-    if not functionTableOption:
-        print('No options available. Please select an option.')
-        return  # Exit the function if there are no options
 
-    tableOptions2 = ttk.Combobox(window, value=functionTableOption)
-    tableOptions2.pack()
-
-    # Define the function to be executed when an option is selected
-    def on_option_selected(event):
-        displayComponents()
+    # Create Dropbox containing all the tables stored in 'functionTableOption'
+    tableOptionComboBox = ttk.Combobox(window, value=functionTableOption)
+    tableOptionComboBox.pack()
 
     # Bind the function to the ComboboxSelected event
-    tableOptions2.bind('<<ComboboxSelected>>', on_option_selected)
-
-    return components
+    tableOptionComboBox.bind('<<ComboboxSelected>>', lambda event: displayComponents(window, event))
 
 
-# def onOptionSelected(event):
-#     global tableOptions2
-#     selectedTable = tableOptions2.get()
-#     print(f"{selectedTable} was selected")
-#
-#     return selectedTable
+# Comment
+def displayComponents(window, event):
+    # Used getAllColumns(Table) function to get all of the columns available
+    # tableOptionComboBox.get() to say what table to look for.
+    columns = getAllColumns(tableOptionComboBox.get())
+
+    # Print tableOptionComboBox.get() for debugging purposes
+    print(f'tableOptionComboBox: {tableOptionComboBox.get()}')
+
+    # Do forLoop to create Labels and Entry per column available in a table
+    for column in columns:
+        # Create Labels with Column Name
+        columnLabel = tk.Label(window, text=f'{column}')
+        columnLabel.pack()
+
+        # Create Entry with Column Name
+        columnEntry = tk.Entry(window)
+        columnEntry.pack()
+        components[column] = columnEntry
+
+    # Create Button and upon left click '<ButtonRelease-1>', go to onAddInventoryPress Function
+    addInventoryButton = tk.Button(window, text='Add to table')
+    addInventoryButton.pack()
+    addInventoryButton.bind('<ButtonRelease-1>', onAddInventoryPress)
 
 
+# Comment
 def onAddInventoryPress(event):
+    # Get the user responses stored in the Entry
     userValues = [entry.get() for entry in components.values()]
+
+    # Print for Debugging purposes
     print(f'components: {components}')
     print(f'userValues: {userValues}')
-    print(f'userOption: {userOption}')
-    addToInventory(userOption, userValues)
+    print(f'AddtoInventory: {tableOptionComboBox.get()}')
 
-    # components[column] = columnEntry
-    # print(columnEntry)
-
-
-def InventoryOptions(event):
-    pass
+    # addToInventory takes two arguments, Table and values for placeholders.
+    # tableOptionComboBox.get() is used to define what table to put the values in
+    # userValues contains the relevant number of values to be used in the placeholder found in SQLQuery
+    addToInventory(tableOptionComboBox.get(), userValues)
 
 
-def onSignUpButtonPress(event):
-    pass
-
-
+# Defines what to run
 def addComponents(window):
     DBInteract(window)
-
-
-getAllTables()
